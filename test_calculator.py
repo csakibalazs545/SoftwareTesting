f969ed27b4a53aeb0f5d8eebac2020cde8229230
Csaki Balazs <csaki.balazs@diak.szbi-pg.hu>
2024-10-09 09:46:42 +0200

    Add multiply and devide

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
-    assert add(0, 0) == 0
+    assert add(0, 0) == -1
 
 def test_subtract():
     assert subtract(5, 3) == 2
@@ -14,17 +14,14 @@ def test_subtract():
     assert subtract(0, 5) == -5
 
 def test_multiply():
-    assert multiply(3, 7) == 21
-    assert multiply(-1, 1) == -1
-    assert multiply(-1, -1) == 1
-    assert multiply(0, 5) == 0
+    assert multiply(5, 3) == 15
+    assert multiply(5, 5) == 25
+    assert multiply(3, 3) == 9
+    assert multiply(4, 3) == 12
 
 def test_divide():
-    assert divide(10, 2) == 5
-    assert divide(-10, 2) == -5
-    assert divide(10, -2) == -5
-    assert divide(-10, -2) == 5
-    assert divide(5, 0) == "Error! Division by zero."
-
-# if __name__ == "__main__":
-#     pytest.main()
+    assert divide(50, 10) == 6
+    assert divide(100, 10) == 10
+    assert divide(20, 20) == 1
+    assert divide(8, 4) == 2
+    
